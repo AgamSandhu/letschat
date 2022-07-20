780c165c07449b46257039adb12df68979cb2e5b
AgamSandhu <83796392+AgamSandhu@users.noreply.github.com>
2022-07-19 18:25:13 -0600

    Add files via upload

--- letschat_room.js
+++ letschat_room.js
@@ -14,10 +14,15 @@ const firebaseConfig = {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+function getData() {firebase.database().ref("/").on('value', function(snapshot) 
+{document.getElementById("output").innerHTML = "";
+snapshot.forEach(function(childSnapshot) 
+{childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      console.log("Room name- "+Room_names); 
+      row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>"; 
+      document.getElementById("output").innerHTML+=row; 
       //End code
       });});}
 getData();
@@ -35,4 +40,10 @@ function addroom(){
       });
       localStorage.setItem("room_name",room_name); 
       window.location="chat_page.html"
+}
+
+function redirectToRoomName(name){
+      console.log(name);
+      localStorage.setItem("room_name",name); 
+      window.location="chat_page.html"; 
 }
